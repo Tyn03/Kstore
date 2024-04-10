eb919cfd064ef8b45b3f8a67b2bd625773ba5d18
unknown <douykhang2002@gmail.com>
2024-04-11 00:16:37 +0200

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,8 +18,8 @@ function App() {
     <div className='app'>
       <BrowserRouter>
       <Navbar/>
-      <Routes>
-        <Route path='/' element={<Shop/>}/>
+      <Routes basename="/Kstore">
+        <Route path='/Kstore' element={<Shop/>}/>
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/women' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}/>
